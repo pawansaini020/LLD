0c7384dd57a50486afaa70614866aeda5a1b6bf5
pawanksaini <pawan.saini@moveinsync.com>
2024-10-23 21:01:50 +0530

    nothing

--- src/main/java/com/pawan/LLD/interview/lldtest/Test.java
+++ src/main/java/com/pawan/LLD/interview/lldtest/Test.java
@@ -69,6 +69,7 @@ public class Test {
     class WhatsappCommValidation implements ICommValidation {
 
         // validate
+        //
     }
 
     class SMSCommValidation implements ICommValidation {
